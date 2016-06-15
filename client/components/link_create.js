ac2a210def054927244de6f2ce170279df881646
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-06-15 13:23:15 -0400

    created Meteor method and linked it to form onSubmit

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    // By default, Meteor ships with the 'insecure package', meaning anyone can manipulate data
+      // To prevent this, we '> meteor remove insecure'
+      // Meteor methods are functions that give us secure ways to add/remove/change our data
+      // This method is defined inside imports/collections/links.js and this is how we call it
+    Meteor.call('links.insert', this.refs.input.value);
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to shorten</label>
-          <input className="form-control" />
+          <input ref="input" className="form-control" />
         </div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,3 +1,12 @@
 import { Mongo } from 'meteor/mongo';
+// When adding a Meteor method, we generally want to define it in the same file that contains the collection it will update
 
-export const Links = new Mongo.collection('links');
+// We will be able to reference links.insert as a function
+    // Named 'links.insert' to make the relationship to collection explicit
+Meteor.methods({
+  'links.insert': function(url) {
+    console.log("saving url", url);
+  }
+});
+
+export const Links = new Mongo.Collection('links');
